3e5535bb4fdace3469fc35ff0706b04050caefce
Baipalli Sagar <baipalli@wednesday.is>
2022-10-04 11:03:58 +0530

    test: updated tests

--- server/gql/tests/queries.test.js
+++ server/gql/tests/queries.test.js
@@ -5,9 +5,10 @@ import { graphql, GraphQLSchema } from 'graphql';
 
 import { QueryRoot } from '../queries';
 import { MutationRoot } from '../mutations';
+import { logger } from '@server/utils';
 
 const schema = new GraphQLSchema({ query: QueryRoot, mutation: MutationRoot });
-const allModels = ['address', 'product', 'purchasedProduct', 'storeProduct', 'store', 'supplierProduct', 'supplier'];
+const allModels = ['author', 'authorsBook', 'book', 'booksLanguage', 'language', 'publisher'];
 
 allModels.forEach(model => allModels.push(pluralize(model)));
 
@@ -24,11 +25,14 @@ describe('query tests', () => {
       }
     }
 `;
+    logger().info('all models', allModels);
+
     const result = await graphql({ schema, source });
     const queryRoot = get(result, 'data.__schema.queryType.fields', []);
     const allQueries = [];
     queryRoot.forEach(query => allQueries.push(query.name));
     const hasModelWithoutQuery = allModels.some(model => !includes(allQueries, model));
+    logger().info('models without queries', hasModelWithoutQuery);
     expect(hasModelWithoutQuery).toBeFalsy();
   });
 });
