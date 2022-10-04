e28f3a3625c8e6ecd5ade6848deb6fec1842f0af
Baipalli Sagar <baipalli@wednesday.is>
2022-10-04 15:00:13 +0530

    feat(updated): added util for daos(update)

--- server/daos/authorsBooks.js
+++ server/daos/authorsBooks.js
@@ -1,14 +1,14 @@
 import db from '@database/models';
 
-export const insertAuthorsBooks = async args => db.authorsBooks.bulkCreate(args);
+export const insertAuthorsBooks = args => db.authorsBooks.bulkCreate(args);
 
-export const updateAuthorsBooksForBooks = async args =>
+export const updateAuthorsBooksForBooks = args =>
   db.authorsBooks.bulkCreate(args, {
     fields: ['id', 'bookId', 'authorId'],
     updateOnDuplicate: ['bookId']
   });
 
-export const updateAuthorsBooksForAuthors = async args =>
+export const updateAuthorsBooksForAuthors = args =>
   db.authorsBooks.bulkCreate(args, {
     fields: ['id', 'bookId', 'authorId'],
     updateOnDuplicate: ['authorId']
--- server/daos/books.js
+++ server/daos/books.js
@@ -1,27 +1,7 @@
 import db from '@database/models';
+import { daosUpdateUtils } from '@server/utils/daosUpdateUtils';
 
 export const insertBook = args => db.books.create(args);
 
-export const updateBook = async ({ id, name, genres, pages, publisherId }, { fetchUpdated }) => {
-  const options = {};
-
-  options.where = { id };
-
-  if (fetchUpdated) {
-    options.returning = true;
-  }
-
-  const bookRes = await db.books.update({ name, genres, pages, publisherId }, options);
-
-  let affectedRowData;
-
-  if (fetchUpdated) {
-    // bookRes = returns an array with one or two elements. The first element is always the number of affected rows, while the second element is the actual affected rows(PostgresSQl only)
-    const affectedRow = bookRes[1];
-
-    // affectedRow = gets an array of all affected row elements(in our case we will always get a single element)
-    affectedRowData = affectedRow[0];
-  }
-
-  return fetchUpdated ? affectedRowData : bookRes;
-};
+export const updateBook = ({ id, name, genres, pages, publisherId }, { fetchUpdated }) =>
+  daosUpdateUtils(db.books, id, { name, genres, pages, publisherId }, fetchUpdated);
