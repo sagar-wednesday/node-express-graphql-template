7c2e4fd85798d7a6944eb10f7c77fe35738058dc
Baipalli Sagar <baipalli@wednesday.is>
2022-10-07 17:46:52 +0530

    feat: removed unused code and improved code coverage

--- server/utils/tests/token.test.js
+++ server/utils/tests/token.test.js
@@ -9,12 +9,26 @@ describe('token tests', () => {
   beforeEach(() => {
     process.env = { ...OLD_ENV, ...keys };
   });
+
   afterAll(() => {
     process.env = OLD_ENV;
   });
-  it('should return error if there is not any user', () => {
+
+  it('should not return error if there any user', () => {
     const user = { id: 1 };
     const signedToken = new Token({ user }).get();
     expect(signedToken).not.toBeUndefined();
+  });
+
+  it('should  return an error if there are not any user', () => {
+    const user = undefined;
+    const signedToken = new Token({ user });
+    expect(signedToken.user).toBeUndefined();
+  });
+
+  it('should take override expiration as args', () => {
+    const overrideExpiration = '2d';
+    const signedToken = new Token({ overrideExpiration });
+    expect(signedToken.expiresIn).toEqual(overrideExpiration);
   });
 });
